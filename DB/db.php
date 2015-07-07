b926594d21f67e32098f71fc212fb52a63fde9b6
ph222222 <philipbelder@gmail.com>
2015-07-08 00:03:41 +0300

    test

--- DB/db.php
+++ DB/db.php
@@ -18,7 +18,7 @@ function connect() {
 
 //connect to a database named "mary" on the host "sheep" with a username and password
 /* 
- * To change  hthis license header, choose License Headers in Project Properties.
+ * To change  this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
